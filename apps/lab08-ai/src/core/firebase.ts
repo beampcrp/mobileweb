78422f8adfbe48d8e9c8dabd8ab8daa4696994a8
beampcrp <beampcrp920@gmail.com>
2026-02-22 21:51:20 +0700

    lab08_ai

---
+++ apps/lab08-ai/src/core/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD8SMsbv9NYHsiZwWn0y1hcvRznW8HZUUs",
+  authDomain: "mobileweb-1ef3e.firebaseapp.com",
+  projectId: "mobileweb-1ef3e",
+  storageBucket: "mobileweb-1ef3e.firebasestorage.app",
+  messagingSenderId: "269504115754",
+  appId: "1:269504115754:web:86db1f8986df1a00d4ff53"
+};
+
+export const app = initializeApp(firebaseConfig);
